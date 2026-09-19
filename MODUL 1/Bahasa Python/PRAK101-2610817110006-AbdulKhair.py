f3bf9f53def717d9fbb3b4114d4ec76b7f87fbae
Abdul Khair <fifayervoid@gmail.com>
2026-09-19 23:09:29 +0800

    Delete MODUL 1 directory

--- MODUL 1/Bahasa Python/PRAK101-2610817110006-AbdulKhair.py
+++
@@ -1,8 +0,0 @@
-x = 5
-y = 7
-z = 9
-
-print(f"Variabel x bernilai {x}")
-print(f"Variabel y bernilai {y}")
-print(f"Variabel z bernilai {z}")
-print(f"Jumlah Variabel tersebut adalah {x + y + z}")
--- MODUL 1/PRAK109-2610817110006-AbdulKhair.py
+++
@@ -1,7 +0,0 @@
-army = 958730
-hero = 5
-fight = army / hero
-
-print(f"Jumlah pasukan yang dibawa Yu Zhong = {army}")
-print(f"Jumlah Pahlawan = {hero}")
-print(f"Jumlah pasukan yang harus dikalahkan setiap pahlawan adalah {fight:.0f}")
